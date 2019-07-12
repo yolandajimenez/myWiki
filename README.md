f113a0fc587fb5a1109bd7fb4befe44ab2866e74
yolanda.jimenez.anaya.contractor <yolanda.jimenez.anaya.contractor@bbva.com>
2019-07-12 11:43:26 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+**My Wiki**
+
+Wiki para apuntes personales, hecha con HTML y JS vanilla
+
+
+**Start**
+
+* npm install
+* gulp watch
---
+++ js/operations.js
@@ -0,0 +1,25 @@
+
+function selectOption(e) {
+  e = e || window.event;
+	let target = e.target || e.srcElement;
+  let myPage = target.attributes.name.value;
+  showPage(myPage);
+}
+
+function showPage(myPage) {
+  var xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+      if (xmlhttp.status == 200) {
+        document.getElementById("textContent").innerHTML = xmlhttp.responseText;
+      }
+      else {
+        document.getElementById("textContent").innerHTML = "No hay datos";
+      }
+    }
+  };
+
+  xmlhttp.open("GET", "../pages/"+ myPage + ".html", true);
+  xmlhttp.send();
+}
